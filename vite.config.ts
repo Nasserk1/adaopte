818275e7eab22a3f27b353fe336c5722ec6912d0
nkh97430@gmail.com <nkh97430@gmail.com>
2026-04-19 00:42:02 +0200

    update tsconfig.app.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
+  root: path.resolve(__dirname, "."),
+  build: {
+    outDir: "dist",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   base: command === "build" ? "/" : "/",
-  server: { 
+  server: {
     watch: { usePolling: true },
     proxy: {
       "/api": "http://localhost:3000"
