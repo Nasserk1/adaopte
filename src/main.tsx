2cb62ba7905f9ba14cdd7a161aff31bff33b6c1e
nkh97430@gmail.com <nkh97430@gmail.com>
2026-04-20 16:58:38 +0200

    update import

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,13 @@
-import Home from "./pages-temp/Home";
-import NotFound from "./pages-temp/NotFound";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 import Layout from "./components/Layout";
-import Jadopte from "./pages-temp/Jadopte";
-import Benevole from "./pages-temp/Benevole";
+import Jadopte from "./pages/Jadopte";
+import Benevole from "./pages/Benevole";
 import "./index.css";
 
 const router = createBrowserRouter([
