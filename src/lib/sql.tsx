185e2bd70f197268a03a7d83c9a0dc50517d36ed
nkh97430@gmail.com <nkh97430@gmail.com>
2026-04-22 13:45:55 +0200

    modif sql.tsx

--- src/lib/sql.tsx
+++ src/lib/sql.tsx
@@ -1,5 +1,5 @@
 export async function sql(query : string, params = []) {
-  const res = await fetch('http://localhost:3001/sql', {
+  const res = await fetch('https://adaopte-api.onrender.com/sql', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ query, params })
